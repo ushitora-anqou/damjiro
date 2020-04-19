3086b0cc44c5e598fc5cc53d081ce55e9781d8d6
Ushitora Anqou (via Travis CI) <ushitora@anqou.net>
2020-04-19 11:46:51 +0000

    Deploy ushitora-anqou/damjiro to github.com/ushitora-anqou/damjiro.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://ushitora-anqou.github.io/damjiro/precache-manifest.0f99ffe26d51cd1b83c0af5332ec8c0c.js"
+  "https://ushitora-anqou.github.io/damjiro/precache-manifest.3e572fe99672542ce7cab1af9a9373ba.js"
 );
 
 self.addEventListener('message', (event) => {
