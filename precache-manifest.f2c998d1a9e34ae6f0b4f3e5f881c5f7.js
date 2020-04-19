3e2d7da2837566639b788f4b22da60b16d7933ac
Ushitora Anqou (via Travis CI) <ushitora@anqou.net>
2020-04-19 11:48:50 +0000

    Deploy ushitora-anqou/damjiro to github.com/ushitora-anqou/damjiro.git:gh-pages

---
+++ precache-manifest.f2c998d1a9e34ae6f0b4f3e5f881c5f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "934ec38851d1f078599ff5b5db749a45",
+    "url": "https://ushitora-anqou.github.io/damjiro/index.html"
+  },
+  {
+    "revision": "6e5fd000dcd454521e64",
+    "url": "https://ushitora-anqou.github.io/damjiro/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7a9934f4ef373ede395d",
+    "url": "https://ushitora-anqou.github.io/damjiro/static/js/2.8a12867b.chunk.js"
+  },
+  {
+    "revision": "6ce3d66cc79125cf77828f8ddc4e2898",
+    "url": "https://ushitora-anqou.github.io/damjiro/static/js/2.8a12867b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e5fd000dcd454521e64",
+    "url": "https://ushitora-anqou.github.io/damjiro/static/js/main.c03e03f8.chunk.js"
+  },
+  {
+    "revision": "75f1942d40228072d411",
+    "url": "https://ushitora-anqou.github.io/damjiro/static/js/runtime-main.3269fb32.js"
+  }
+]);
